36a3d945910e25869e4ed744e1a84bc3b3fbddc1
abhigoudar <deloney21@gmail.com>
2023-11-09 20:37:07 -0500

    Added some launch files and some preliminary testing

---
+++ fast_planner/plan_manage/launch/fast_planner.launch.py
@@ -0,0 +1,10 @@
+from launch_ros.actions import Node
+from launch.launch_description import LaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    launch_description = []
+    
+    return LaunchDescription(launch_description)
